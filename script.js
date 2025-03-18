77de9a23cf43b7135aa697771d49db1133883c03
Prajwal <prajwal.gujar@barquecontech.com>
2025-03-18 13:24:49 +0530

    new changes are added

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleButton = document.getElementById("theme-toggle");
-    toggleButton.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
-    });
-});
